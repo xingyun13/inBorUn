9da79eac51f207d30955a26034082d51592b2a3d
彭帅 <2077796713@qq.com>
2026-06-04 16:43:20 +0800

    fix error

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,12 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-      colors: {
-        tech: {
-          50: '#f0f9ff',
-          100: '#e0f2fe',
-          500: '#0ea5e9',
-          600: '#0284c7',
-          800: '#075985',
-          900: '#0c4a6e',
-        }
-      }
-    },
+      
+    }
   },
   plugins: [],
 }
